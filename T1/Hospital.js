c6450ddc030336c127bf98fbfda5b764f05a435b
Rana <161890296+RanaTarekAhmed@users.noreply.github.com>
2026-06-29 00:44:52 +0300

    Add hospital function for patient treatment categorization
    Implement hospital function to categorize patients based on condition and data availability.

--- T1/Hospital.js
+++ T1/Hospital.js
@@ -1,1 +1,39 @@
+function hospital(patients)
+{
+    let treatedImmediately = [];
+    let normalTreated = [];
+    let missingDataList = [];
+    for (let i = 0; i < patients.length; i++)
+    {
+        if (patients[i].hasData === false)
+        {
+            missingDataList.push(patients[i]);
+            continue;
+        }
+        if (patients[i].condition === "critical") treatedImmediately.push(patients[i]);
+        else normalTreated.push(patients[i]);
+    }
+    for (let i = 0; i < normalTreated.length-1; i++)
+    {
+    for (let j = 0; j < normalTreated.length-1-i; j++)
+    {
+        if (normalTreated[j].severity < normalTreated[j+1].severity) [normalTreated[j], normalTreated[j+1]] = [normalTreated[j+1], normalTreated[j]];
+    }
+    }
+    return {treatedImmediately, normalTreated, missingDataList};
+}
 
+
+
+let patients = 
+[
+    {name:"A", severity:2, hasData:true, condition:"normal"},
+    {name:"B", severity:5, hasData:true, condition:"critical"},
+    {name:"C", severity:4, hasData:false, condition:"normal"},
+    {name:"D", severity:3, hasData:true, condition:"normal"},
+    {name:"E", severity:5, hasData:true, condition:"normal"}
+];
+let r = hospital(patients);
+console.log(r.treatedImmediately);
+console.log(r.normalTreated);
+console.log(r.missingDataList);
