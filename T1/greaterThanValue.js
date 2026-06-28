f37b585ec86d8da86a665b1f552632433b1dc9f4
Rana <161890296+RanaTarekAhmed@users.noreply.github.com>
2026-06-29 00:48:35 +0300

    Implement greaterThan function to filter array

--- T1/greaterThanValue.js
+++ T1/greaterThanValue.js
@@ -1,1 +1,11 @@
+function greaterThan(arr, value)
+{
+    let r = [];
+    for (let i = 0; i < arr.length; i++)
+    {
+        if (arr[i] > value) r.push(arr[i]);
+    }
+    return r;
+}
 
+console.log(greaterThan([2,5,8,1,9,3.10],4));
