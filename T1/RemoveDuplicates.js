398ea0322e5019f704b31fb2cf322dbe1d5b3e21
Rana <161890296+RanaTarekAhmed@users.noreply.github.com>
2026-06-29 01:09:06 +0300

    Add removeDuplicates function to filter unique values
    Implement function to remove duplicates from sorted array.

--- T1/RemoveDuplicates.js
+++ T1/RemoveDuplicates.js
@@ -1,1 +1,19 @@
+function removeDuplicates(nums)
+{
+    if (nums.length === 0)return 0;
+    let r = 1;
+    for (let i = 1; i < nums.length; i++)
+    {
+        if (nums[i] !== nums[r-1])
+        {
+            nums[r] = nums[i];
+            r++;
+        }
+    }
+    return r;
+}
 
+let nums = [1,1,2,2,3];
+let n = removeDuplicates(nums);
+console.log(n);
+for (let i = 0; i < n; i++) console.log(nums[i]);
