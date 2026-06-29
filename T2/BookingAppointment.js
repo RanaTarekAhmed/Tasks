97e09e0ceb41df9db7a901f040b5b4b68fd80ee1
Rana <161890296+RanaTarekAhmed@users.noreply.github.com>
2026-06-29 22:37:17 +0300

    Implement appointment booking with slot checks
    Added booking functionality with promise handling for appointment slots.

--- T2/BookingAppointment.js
+++ T2/BookingAppointment.js
@@ -1,1 +1,35 @@
+const bookedSlots = ["a1", "b3"];
 
+function bookAppointment(slot)
+ {
+    return new Promise((resolve, reject) => 
+    {
+        setTimeout(() => 
+        {
+            if (bookedSlots.includes(slot)) {
+                reject(`Failed to book slot "${slot}" is already booked!`);
+            } else {
+                bookedSlots.push(slot);
+                resolve(`Appointment booked successfully for slot "${slot}"!`);
+            }
+
+        }, 3000);
+    });
+}
+
+async function booking(slot) 
+{
+    try 
+    {
+        const message = await bookAppointment(slot);
+        console.log(message);
+    } 
+    catch (error)
+    {
+        console.log(error);
+    }
+}
+
+booking("a1"); // Rejected
+booking("b3"); // Rejected
+booking("a2"); // Successfully
