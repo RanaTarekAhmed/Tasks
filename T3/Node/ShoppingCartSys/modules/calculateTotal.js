c4771527d31637297ecba30c6ccae2d446fe5717
Rana <161890296+RanaTarekAhmed@users.noreply.github.com>
2026-06-30 19:11:21 +0300

    Add calculateTotal function to compute cart total

---
+++ T3/Node/ShoppingCartSys/modules/calculateTotal.js
@@ -0,0 +1,11 @@
+const cart = require("../data/cart");
+
+function calculateTotal() {
+    let total = 0;
+    cart.forEach(product => {
+        total += product.price;
+    });
+    console.log(`Total: $${total}`);
+}
+
+module.exports = calculateTotal;
