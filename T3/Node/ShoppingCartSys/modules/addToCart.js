742c5752d3218d9ab863f88ffb3b24775b4e8c39
Rana <161890296+RanaTarekAhmed@users.noreply.github.com>
2026-06-30 19:07:53 +0300

    Implement addToCart function to manage cart items

--- T3/Node/ShoppingCartSys/modules/addToCart.js
+++ T3/Node/ShoppingCartSys/modules/addToCart.js
@@ -1,1 +1,15 @@
+const products = require("../data/products");
+const cart = require("../data/cart");
 
+function addToCart(id) {
+    const product = products.find(product => product.id === id);
+    if (!product) 
+    {
+        console.log("Product not found!\n");
+        return;
+    }
+    cart.push(product);
+    console.log(`${product.name} added to cart.\n`);
+}
+
+module.exports = addToCart;
